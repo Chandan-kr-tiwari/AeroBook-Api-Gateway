eeecd8fe8bc7618735c291d509e83d105e1a1312
Chandan Tiwari <chandan.kr.tiwarii@gmail.com>
2026-09-14 06:52:47 +0000

    fixed the bug in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,35 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const {
-   AEROBOOK_USER_SERVICE,
-   AEROBOOK_FLIGHT_SERVICE,
-   AEROBOOK_BOOKING_SERVICE,
-   AEROBOOK_PAYMENT_SERVICE,
+    AEROBOOK_USER_SERVICE,
+    AEROBOOK_FLIGHT_SERVICE,
+    AEROBOOK_BOOKING_SERVICE,
+    AEROBOOK_PAYMENT_SERVICE,
 } = require("../config/server-config");
 
 const router = express.Router();
+
+router.use(
+    "/users",
+    createProxyMiddleware({
+        target: AEROBOOK_USER_SERVICE,
+        changeOrigin: true,
+        pathRewrite: {
+            "^/": "/api/v1/users/",
+        },
+    })
+);
+
+router.use(
+    "/auth",
+    createProxyMiddleware({
+        target: AEROBOOK_USER_SERVICE,
+        changeOrigin: true,
+        pathRewrite: {
+            "^/": "/api/v1/auth/",
+        },
+    })
+);
 
 router.use(
     "/flights",
@@ -16,8 +38,8 @@ router.use(
         target: AEROBOOK_FLIGHT_SERVICE,
         changeOrigin: true,
         pathRewrite: {
-            "^/flights": "/api/v1"
-        }
+            "^/": "/api/v1/flights/",
+        },
     })
 );
 
@@ -27,8 +49,8 @@ router.use(
         target: AEROBOOK_BOOKING_SERVICE,
         changeOrigin: true,
         pathRewrite: {
-            "^/bookings": "/api/v1"
-        }
+            "^/": "/api/v1/bookings/",
+        },
     })
 );
 
@@ -38,19 +60,8 @@ router.use(
         target: AEROBOOK_PAYMENT_SERVICE,
         changeOrigin: true,
         pathRewrite: {
-            "^/payments": "/api/v1"
-        }
-    })
-);
-
-router.use(
-    "/users",
-    createProxyMiddleware({
-        target: AEROBOOK_USER_SERVICE,
-        changeOrigin: true,
-        pathRewrite: {
-            "^/users": "/api/v1"
-        }
+            "^/": "/api/v1/payments/",
+        },
     })
 );
 
